fc0181aca2271168e40340ab0aeb5bba80b7436f
n4m1t4q <taku.chiki.apple@gmail.com>
2020-06-18 21:33:26 +0900

    Step 4

---
+++ src/app/todo-form/todo-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent implements OnInit {
+  title: string;
+
+  @Output() submit = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  create() {
+    this.submit.emit(this.title);
+    this.title = '';
+  }
+
+}
